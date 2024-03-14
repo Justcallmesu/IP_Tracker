ec25e7d07d19cd37b9d93edd59ce6f21af7d4668
codenoobies <kevindubidaam@gmail.com>
2024-03-14 15:47:42 +0700

    create:Created Hero.tsx File

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 
+import { Hero } from "./components/Layout/Hero"
 
 export function App(){
-
-
     return(
-        <>
-        
-        </>
+        <div className="h-screen flex flex-col">
+            <Hero/>
+        </div>
     )
 }
