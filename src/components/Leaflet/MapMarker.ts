2dc622d41b4e28e53dbffb4e0a133ef630733d54
codenoobies <kevindubidaam@gmail.com>
2024-03-15 15:59:44 +0700

    feat: Changed to a custom Marker

---
+++ src/components/Leaflet/MapMarker.ts
@@ -0,0 +1,11 @@
+import {Icon}from 'leaflet'
+
+
+import IconSVG from "../../Assets/images/icon-location.svg"
+export const MarkerIcon = new Icon(
+    {
+        iconUrl:IconSVG,
+        iconSize:[50,64]
+    }
+)
+
